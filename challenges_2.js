0184aef440693f2b2234ef538c81c93f57ecfa96
christian <christianbiringanine22@gmail.com>
2022-08-27 20:02:47 +0200

    Birthday bar challenge hackerrank

--- challenges_2.js
+++ challenges_2.js
@@ -55,3 +55,25 @@ function StringChallenge(str) {
   }
   return str;
 }
+
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(s, d, m) {
+    // Write your code here
+    let result = 0;
+    for(let i = 0; i < s.length; i++) {
+        if (s.slice(i, i + m).reduce((r, v) => r + v, 0) === d) {
+            result++;
+        }
+    }
+    return result;
+}
